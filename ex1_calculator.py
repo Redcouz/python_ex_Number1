3445c871585dd3cb69fa5df0f93b2c5bbe6f238c
Redcouz <63596208+Redcouz@users.noreply.github.com>
2020-05-18 10:26:44 -0300

    Update ex1_calculator.py

--- ex1_calculator.py
+++ ex1_calculator.py
@@ -9,22 +9,31 @@ def performMath():
     global run
     global previous
     equation = ''
-    if previous == 0: #<<<
+    if previous == 0: #<<< Make sure thats starts at the first equation >>>
+        
         equation = input('Enter Equation (Type "Quit" if you want to finish):')
-    else:
+        
+    else: #<<< Sum to the previous result >>>
+        
         equation = input(str(previous))
-    if equation == "Quit":
+        
+    if equation == "Quit": #<<< Ends the script >>>
+        
         run = False
         print("Thanks for using may calculator, See you soon")
-    else:
-        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+        
+    else: 
+        
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation) #<<< Delete everything that is not a number >>>
         if previous == 0:
+            
             previous = eval(equation)
 
         else:
+            
             previous = eval(str(previous) + equation)
 
-        print("Its equal to", previous)
+        print("Its equal to", previous) #<<< Outcome >>>
 
 while run:
     performMath()
